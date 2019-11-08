5be69f2366eb04fcd281d09a4c453169073652e2
AnthonyKenny98 <anthony_kenny@college.harvard.edu>
2019-11-08 11:28:08 -0500

    Move app.py to src

--- app.py
+++
@@ -1,8 +0,0 @@
-"""Main Application."""
-# -*- coding: utf-8 -*-
-# @Author: AnthonyKenny98
-# @Date:   2019-11-08 10:28:57
-# @Last Modified by:   AnthonyKenny98
-# @Last Modified time: 2019-11-08 10:34:16
-
-import requests
