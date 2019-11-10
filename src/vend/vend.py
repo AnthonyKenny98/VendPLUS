74a180b13700b5760451891386084d33d7afbd87
AnthonyKenny98 <anthony_kenny@college.harvard.edu>
2019-11-10 14:39:28 -0500

    Add Example Credentials

--- src/vend/vend.py
+++ src/vend/vend.py
@@ -3,10 +3,23 @@
 # @Author: AnthonyKenny98
 # @Date:   2019-11-10 14:09:50
 # @Last Modified by:   AnthonyKenny98
-# @Last Modified time: 2019-11-10 14:11:26
+# @Last Modified time: 2019-11-10 14:30:36
+
+import os
+
+REDIRECT_URI = 'http://127.0.0.0:5000/token'
+
 
 class Vend:
-    """Provide Class for interfacing with Vend API"""
+    """Provide Class for interfacing with Vend API."""
 
     def __init__(self):
-        
+        """Initialise Class."""
+        # Get filepath
+        pyfile_path = os.path.dirname(os.path.realpath(__file__))
+
+        # Init Credentials
+        with open(pyfile_path + '/client_id.credentials', 'r') as f1, \
+                open(pyfile_path + '/client_secret.credentials', 'r') as f2:
+            self.client_id = f1.read()
+            self.client_secret = f2.read()
--- src/vend/vend_test.py
+++ src/vend/vend_test.py
@@ -3,4 +3,4 @@
 # @Author: AnthonyKenny98
 # @Date:   2019-11-10 14:11:36
 # @Last Modified by:   AnthonyKenny98
-# @Last Modified time: 2019-11-10 14:11:45
+# @Last Modified time: 2019-11-10 14:37:25
