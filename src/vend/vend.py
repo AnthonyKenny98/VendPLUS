52a28665b5caa2ab4f2307519136ce70004fa581
AnthonyKenny98 <anthony_kenny@college.harvard.edu>
2019-11-10 14:20:11 -0500

    added .credentials files to gitignore

---
+++ src/vend/vend.py
@@ -0,0 +1,12 @@
+"""Vend.py."""
+# -*- coding: utf-8 -*-
+# @Author: AnthonyKenny98
+# @Date:   2019-11-10 14:09:50
+# @Last Modified by:   AnthonyKenny98
+# @Last Modified time: 2019-11-10 14:11:26
+
+class Vend:
+    """Provide Class for interfacing with Vend API"""
+
+    def __init__(self):
+        
---
+++ src/vend/vend_test.py
@@ -0,0 +1,6 @@
+"""Vend_test.py."""
+# -*- coding: utf-8 -*-
+# @Author: AnthonyKenny98
+# @Date:   2019-11-10 14:11:36
+# @Last Modified by:   AnthonyKenny98
+# @Last Modified time: 2019-11-10 14:11:45
